06a5c76059e48b2c6fef171343ddda67cf5e5571
Leonardo Eneas <leonardo.neves@ilia.digital>
2022-01-20 21:24:13 -0300

    :tada: Setting the basic's project structure

---
+++ src/components/shared/header/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <router-link :to="{ name: 'home' }">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
